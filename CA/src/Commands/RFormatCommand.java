77d66ba2013db370085ca9f184acf1a86eee37fd
Ahmad Soliman <ahmadadel922gmail.com>
2013-04-21 14:47:24 +0200

    Initial Commit

---
+++ CA/src/Commands/RFormatCommand.java
@@ -0,0 +1,5 @@
+package Commands;
+
+public abstract class RFormatCommand extends IRFormatCommand{
+
+}
---
+++ CA/src/Utilities/BinaryUtilities.java
@@ -0,0 +1,15 @@
+package Utilities;
+
+public class BinaryUtilities {
+	
+	public static int convertBinaryToDecimal(String binary){
+		
+		return 0;
+	}
+	
+	public static String convertDecimalToBinary(int decimal){
+		
+		return "";
+	}
+
+}
