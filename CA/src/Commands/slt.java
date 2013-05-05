5c146cb82196afeac2c98fd031f8dfdd94f3ab87
Mirna Yacout <mirna.yacout@gmail.com>
2013-05-05 15:24:51 +0200

    slt modified

--- CA/src/Commands/slt.java
+++ CA/src/Commands/slt.java
@@ -3,7 +3,7 @@ package Commands;
 import DatapathComponents.ALU;
 import DatapathComponents.Register;
 
-public class slt {
+public abstract class slt extends RFormatCommand  {
 	public slt(Register rd, Register rs, Register rt) {
 		rd.setValue(ALU.setLessThan(rs, rt));
 	}
