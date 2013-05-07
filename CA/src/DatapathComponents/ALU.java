42080487bc934081eee05ed7646ec53604e4b30a
Mirna Yacout <mirna.yacout@gmail.com>
2013-05-07 23:04:28 +0200

    alu

--- CA/src/DatapathComponents/ALU.java
+++ CA/src/DatapathComponents/ALU.java
@@ -4,15 +4,15 @@ public class ALU {
 
 	public ALU() {
 	}
-	
-	public static int add(int r1, int r2){
+
+	public static int add(int r1, int r2) {
 		return r1 + r2;
 	}
-	
-	public static int subtract(int r1, int r2){
+
+	public static int subtract(int r1, int r2) {
 		return r1 - r2;
 	}
-	
+
 	public static int setLessThan(int r1, int r2) {
 		if (r1 < r2) {
 			return 1;
@@ -20,17 +20,17 @@ public class ALU {
 			return 0;
 		}
 	}
-	
+
 	public static int and(int r1, int r2) {
-		return  r1 & r2;
+		return r1 & r2;
 	}
-	
+
 	public static int or(int r1, int r2) {
-		return  r1 | r2;
+		return r1 | r2;
 	}
-	
+
 	public static int nor(int r1, int r2) {
-		return  ~(r1 | r2);
+		return ~(r1 | r2);
 	}
-	
+
 }
