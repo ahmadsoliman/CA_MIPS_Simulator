ed4f7f582b67c9bae18f9f3e410f4ffa5e643ac2
Mirna Yacout <mirna.yacout@gmail.com>
2013-05-08 02:24:00 +0200

    using ALU in bne and beq

--- CA/src/Commands/bne.java
+++ CA/src/Commands/bne.java
@@ -1,11 +1,15 @@
 package Commands;
 
+import DatapathComponents.ALU;
 import DatapathComponents.Label;
 import DatapathComponents.Register;
 
-public abstract class bne extends IFormatCommand{
+public abstract class bne extends IFormatCommand {
+	int sub;
+
 	public bne(Register rs, Register rt, Label L1) {
-		if (rs.getValue() != rt.getValue()) {
+		sub = ALU.subtract(rs.getValue(), rt.getValue());
+		if (sub != 0) {
 			// branch to L1
 		}
 	}
